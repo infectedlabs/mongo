08fe2abff90a6084364ea329c5a190289edbd5c7
infectedlabs <117656798+infectedlabs@users.noreply.github.com>
2023-03-11 14:09:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,26 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 
-// add cors middleware
 app.use(cors());
-
-const port = process.env.PORT || 3001;
-
-mongoose.connect('mongodb://localhost:27017/userdata', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const userDataSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-});
-
-const UserData = mongoose.model('UserData', userDataSchema);
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
-app.post('/api/users', async (req, res) => {
-  const { name, email } = req.body;
-  const userData = new UserData({ name, email });
-  try {
-    await userData.save();
-    res.status(201).send({ message: 'User data saved successfully.' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send({ error: 'Error saving user data.' });
+let userData = [];
+
+app.get('/api/users', (req, res) => {
+  res.json(userData);
+});
+
+app.post('/api/users', (req, res) => {
+  const { walletAddress, discordId, discordUsername } = req.body;
+  if (!walletAddress || !discordId || !discordUsername) {
+    res.status(400).json({ message: 'Missing user data' });
+  } else {
+    userData.push({ walletAddress, discordId, discordUsername });
+    res.json({ message: 'User data saved successfully' });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+app.listen(5000, () => {
+  console.log('Server listening on port 5000');
 });
